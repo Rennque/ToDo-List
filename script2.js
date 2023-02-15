f25d3d6fea9e43438da308cc8b9092d3ba77656a
Rennque <47489722+Rennque@users.noreply.github.com>
2023-02-15 00:01:04 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,22 @@
+const tarefas = ["tomarBanho","comer","estudar","dormir"]
+ 
+/*Função que busca se a tarefa existe na lista... 
+console.log(tarefas.every((dia) => {
+        return dia == "varrer"
+ }))  */
+
+
+/*  Função que adiciona um item na lista
+ tarefas.push("varrer");
+ console.log(tarefas);
+ */
+
+ /* Função que altera um item da lista.
+ tarefas.splice(2,0,"academia");
+ console.log(tarefas); */
+
+ 
+/*  Função que deleta um item da lista
+ tarefas.pop();
+ console.log(tarefas) */
+
